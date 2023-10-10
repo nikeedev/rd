fe02b76eafffd83ac83c8fc1c983191be342d4d6
nikeedev <nikita.gon.dev@gmail.com>
2023-10-10 19:58:31 +0200

    updates

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 Read directories better with Rust!
 
+First version
 ![](assets/terminal1.png)
+
+Updated (second) version
+![](assets/terminal2.png)
+
+
+Here is how it looks like from my website repo
+
+![](assets/terminal3.png)
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::{
     env,
     ffi::OsString,
     fs::{self, Metadata},
-    io,
+    io
 };
 
 #[derive(Debug)]
@@ -19,18 +19,10 @@ impl DirInfo {
     fn format(&self) -> String {
         let metadata = self.metadata.as_ref().unwrap();
 
-        let can_write: ColoredString = match !metadata.permissions().readonly() {
-            true => "true".bright_green(),
-            false => "false".bright_red(),
-        };
-
-        let size = metadata.len();
+        let size = format!("{}", metadata.len() as f32 / 1000.0);
 
         let modify: DateTime<Utc> = metadata.modified().unwrap().into();
-        let edited = modify.format("%d/%m/%Y %H:%M");
-
-        let create: DateTime<Utc> = metadata.created().unwrap().into();
-        let created = create.format("%d/%m/%Y %H:%M");
+        let edited = format!("{}", modify.format("%d/%m/%Y %H:%M"));
 
         let file_ext: OsString = if self.file_ext.is_none() {
            self.name.to_owned()
@@ -41,33 +33,51 @@ impl DirInfo {
         let icon = match file_ext.to_str().unwrap() {
             "md" => "".bright_blue(),
             ".gitignore" | ".git" => "".bright_red(),
-            "rs" => "".red(),
-            ".github" => "".white(),
-            "src" => "".white(),
-            "exe" | "o" => "".white(),
+            "rs" | "rust" => "".red(),
+            ".github" => "".bright_white(),
+            "src" => "".bright_white(),
+            "exe" | "o" => "".bright_white(),
             "toml" => "".red(),
             "lock" => "".yellow(),
-            "js" => "".yellow(),
+            "js" | "javascript" => "".yellow(),
             "cpp" => "".blue(),
             "h" => "H".blue(),
             "c" => "".blue(),
             "rain" => "".bright_blue(),
-            _ => " ".white(),
+            "html" => "".bright_red(),
+            "txt" => "".bright_white(),
+            "css" => "".bright_blue(),
+            "LICENSE" => "".red(),
+            "CNAME" => "".red(),
+            _ => " ".bright_white(),
         };
-        let dir_icon = match self.is_dir {
-            true => "",
-            false => "",
+        let dir_icon = if self.is_dir {
+            ""
+        } else {
+            ""
+        };
+
+        let size_text = if !self.is_dir {
+            format!("{} ", size.clone())
+        } else {
+            "      ".to_string()
+        };
+     
+        let num = if (size_text.len()-4) > 2 || (size_text.len()-4) < 2 {
+            size_text.len()-6
+        } else {
+            0
         };
 
         format!(
-            "{} {} {}{}\n\t-- Last edited: {}\n\t-- Can write: {}\n\t-- Created: {}\n",
+            "{}{}{}{}{} {} {}",
+            edited.blue(),
+            " ".repeat(10-(size_text.len()-4)),
+            size_text,
+            " ".repeat(size_text.len()-num),
             icon,
             self.name.to_str().unwrap(),
             dir_icon,
-            format!("\n\t-- 💾 Size (bytes): {size}"),
-            edited,
-            can_write,
-            created
         )
     }
 }
@@ -81,7 +91,8 @@ fn main() -> io::Result<()> {
         dir = &args[1];
     }
 
-    println!("Dir: {}", dir.to_string().bright_green().bold());
+    let current_dir = env::current_dir().unwrap();
+    println!("Current dir: {}\nDir: {}", current_dir.display(), dir.to_string().bright_green().bold());
 
     // let mut entries = fs::read_dir(dir)?
     //     .map(|res| res.map(|e| e.path()))
@@ -97,7 +108,9 @@ fn main() -> io::Result<()> {
             })
         })
         .collect::<Result<Vec<DirInfo>, io::Error>>();
-
+    
+    println!("Last edited           💾 Size (KB)   Name");
+    println!("-----------           ------------   ---------");
     match entries {
         Ok(ent) => {
             for file in ent {
